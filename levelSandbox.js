901e644fcb7dae39d2efd2890d781804fbd06748
Antoine Solomon <asolomon15@gmail.com>
2018-11-11 09:33:55 -0800

    Now each leveldb function now returns a Promise object. This will be useful for calling the functions in an async way with nodejs.

--- levelSandbox.js
+++ levelSandbox.js
@@ -7,32 +7,79 @@ const chainDB = './chaindata';
 const db = level(chainDB);
 
 // Add data to levelDB with key/value pair
-function addLevelDBData(key,value){
-  db.put(key, value, function(err) {
-    if (err) return console.log('Block ' + key + ' submission failed', err);
-  })
+function addLevelDBData(key,value) {
+  return new Promise(function(resolve, reject) {
+    db.put(key, value, function(err) {
+      if (err) {
+        console.log('Block ' + key + ' submission failed', err);
+        reject(err);
+      }
+      resolve(key, value);
+    });
+  });
 }
+
+
 
 // Get data from levelDB with key
 function getLevelDBData(key){
-  db.get(key, function(err, value) {
-    if (err) return console.log('Not found!', err);
-    console.log('Value = ' + value);
-  })
+  return new Promise(function(resolve, reject) {
+    db.get(key, function(err, value) {
+      if (err) {
+        console.log('Not found!', err);
+        reject(err)
+      }
+      console.log('Value = ' + value);
+      resolve(value)
+    });
+  });
 }
 
 // Add data to levelDB with value
 function addDataToLevelDB(value) {
     let i = 0;
-    db.createReadStream().on('data', function(data) {
+    return new Promise(function(resolve, reject) {
+      db.createReadStream()
+        .on('data', function(data) {
           i++;
-        }).on('error', function(err) {
-            return console.log('Unable to read data stream!', err)
-        }).on('close', function() {
+        })
+        .on('error', function(err) {
+          console.log('Unable to read data stream!', err)
+          reject(err)
+        })
+        .on('close', function() {
           console.log('Block #' + i);
-          addLevelDBData(i, value);
+          addLevelDBData(i, value).then((key) => {
+            console.log('Block #' + key);
+          }).catch((err) => {
+            console.log(err);
+          });
+          resolve(value);
         });
+      });
 }
+
+// Get the count of all keys within the database
+function getLevelDBCount() {
+  return new Promise(function(resolve, reject) {
+    let count = 0
+    db.createReadStream()
+      .on('data', function(data) {
+        count++
+      })
+      .on('error', function(err) {
+        console.log(err)
+        reject(err)
+      })
+      .on('close', function() {
+        resolve(count)
+      })
+      .on('end', function() {
+        console.log("Closing stream")
+      })
+  });
+}
+
 
 /* ===== Testing ==============================================================|
 |  - Self-invoking function to add blocks to chain                             |
@@ -45,10 +92,28 @@ function addDataToLevelDB(value) {
 |     ( new block every 10 minutes )                                           |
 |  ===========================================================================*/
 
+/* ===== exporting these functions for the simplechain ========================|
+|*/
+module.exports = {
+  addBlockData: function(key, value) {
+    addLevelDBData(key,value)
+  },
+  getBlockData: function(key) {
+    getLevelDBData(key)
+  },
+  addBlockDataByValue: function(value) {
+    addDataToLevelDB(value)
+  },
+  getBlockLength: function() {
+    getLevelDBCount()
+  }
+};
 
 (function theLoop (i) {
   setTimeout(function () {
-    addDataToLevelDB('Testing data');
+    addDataToLevelDB("Testing data").then((key) => {
+      console.log('key: ' + key);
+    }).catch((err) => {console.log(err); });
     if (--i) theLoop(i);
   }, 100);
 })(10);
